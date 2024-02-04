714770c5cf8a6a2956a076c18b25af4f4b0e45a8
Kavitha Kesavalu <kavithakesavalu@Sakthis-MacBook-Air.local>
2024-02-04 10:50:28 -0500

    feat: create and list issues

--- src/cli.ts
+++ src/cli.ts
@@ -22,7 +22,8 @@ import prStatus from './commands/pr-status.js';
 import push from './commands/push.js';
 import reset from './commands/reset.js';
 import version from './commands/version.js';
-
+import issue from './commands/issue';
+import listIssues from './commands/list-issues.js';
 import ConsoleLogger from './logger/index.js';
 
 const program = new Command();
@@ -151,6 +152,10 @@ addCommand('pr-status', 'Check the status of all PRs for the specified migration
 
 // These commands don't take --repos arguments
 addCommand('list', 'List all checked out repositories for the given migration', false, list);
+
+addCommand('issue', 'open an issue for the specified repos', true, issue);
+
+addCommand('list-issues', 'List all opened issues using migration', true, listIssues);
 
 program
   .command('version')
